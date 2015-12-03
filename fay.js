c31bd784e8878f3712ebbe9eefd294a8651c6bb9
Fengda HUANG <h@phodal.com>
2015-12-03 09:07:48 +0800

    Add download tasks

---
+++ fay.js
@@ -0,0 +1,71 @@
+var os = require('os'),
+    path = require('path'),
+    unzip = require('unzip'),
+    colors = require('colors'),
+    fs = require('fs'),
+    request = require('request'),
+    Q = require('q');
+
+colors.setTheme({
+    silly: 'rainbow',
+    input: 'grey',
+    small: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    info: 'white',
+    data: 'grey',
+    help: 'cyan',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
+});
+
+
+var FAY = {
+    /**
+     * Fetch a repo from GitHub, unzip it to a specific folder.
+     *
+     * The MIT License (MIT)
+     *
+     * Copyright (c) 2013 Drifty
+     * https://github.com/driftyco/ionic-cli
+     */
+
+    fetchRepo: function (targetPath, repoName, repoUrl) {
+        var q = Q.defer();
+
+        var proxy = process.env.PROXY || null;
+
+        // The folder name the project will be downloaded and extracted to
+        var repoFolderName = repoName + '-master';
+        console.log('\nDOWNLOADING:'.info.bold, repoUrl);
+
+        var tmpFolder = os.tmpdir();
+        var tempZipFilePath = path.join(tmpFolder, repoName + new Date().getTime() + '.zip');
+        var tempZipFileStream = fs.createWriteStream(tempZipFilePath);
+
+        var unzipRepo = function (fileName) {
+            var readStream = fs.createReadStream(fileName);
+
+            var writeStream = unzip.Extract({path: targetPath});
+            writeStream.on('close', function () {
+                q.resolve(repoFolderName);
+            });
+            readStream.pipe(writeStream);
+        };
+
+        request({url: repoUrl, encoding: null, proxy: proxy}, function (err, res, body) {
+            if (res.statusCode !== 200) {
+                q.reject(res);
+                return;
+            }
+            tempZipFileStream.write(body);
+            tempZipFileStream.close();
+            unzipRepo(tempZipFilePath);
+        });
+
+        return q.promise;
+    }
+};
+
+//FAY.fetchRepo('fay', 'echeveria-content', 'https://github.com/phodal-archive/echeveria-content/archive/master.zip');
